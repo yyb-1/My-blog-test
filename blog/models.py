9daec6a433f784ae305a0ac1e67d81364f04d382
David <2908113940@qq.com>
2020-12-14 13:55:47 +0800

    完善网站

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Blog(models.Model):
     title = models.CharField(default='文章标题', max_length=50)
     date = models.DateField()
     image = models.ImageField(default='default.png', upload_to='image/')
-    text = models.CharField(default='正文', max_length=1000)
+    text = models.TextField(default='正文')
 
     def __str__(self):
         return self.title
